6d1d2e8b8d448b8bf389ee93053ff9b3ba12f388
Ayush Purohit <ayush.p1@ahduni.edu.in>
2023-08-18 13:21:51 +0530

    Did subsets with recursion

--- assignments/09.08.2023/78.cpp
+++ assignments/09.08.2023/78.cpp
@@ -8,7 +8,7 @@ public:
 
     vector<vector<int>> ans;
 
-    void sub(int i, vector<int>& nums, vector<int> temp){
+    void sub(int i, vector<int>& nums, vector<int>& temp){
         if(i==nums.size()){
             ans.push_back(temp);
             return;
@@ -17,6 +17,7 @@ public:
         sub(i+1,nums,temp);
         temp.push_back(nums[i]);
         sub(i+1,nums,temp);
+        temp.pop_back();
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
