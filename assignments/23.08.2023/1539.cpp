1cd7930597d617a7c16debec11bdcf6d77c82ee4
Ayush Purohit <ayush.p1@ahduni.edu.in>
2023-08-26 13:38:05 +0530

    Removed ans variable since it was unused

--- assignments/23.08.2023/1539.cpp
+++ assignments/23.08.2023/1539.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int ans, ch = 0;
+        int ch = 0;
         for(int i = 0; i < arr.size(); i++){
             if (arr[i] - ch > 1 && arr[i] - ch <= k){
                 k -= arr[i] - ch - 1;
